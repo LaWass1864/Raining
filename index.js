f5556f6a20a60c53c37eaeef616c91f1d9f6cc79
LaWass1864 <amwass4@hotmail.com>
2023-06-19 17:23:54 +0200

    scroll Ok

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const nav = document.querySelector("nav");
+let lastScroll = 0;
+let playOnce = true;
+const popUp = document.getElementById("popup");
 
-window.addEventListener('click', () => {
-    nav.style.background = 'red';
-})
+window.addEventListener("scroll", () => {
+    // Navbar effect
+    if (window.scrollY > 50) {
+        nav.style.background = "red";
+    } else {
+        nav.style.backdropFilter = "invert(80%)";
+    }
+  
+    let scrollValue =
+      (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+    console.log(scrollValue);
+    
+    // Popup
+    if (scrollValue > 0.85 && playOnce) {
+      popup.style.opacity = 1;
+      popup.style.transform = "none";
+      playOnce = false;
+    }
+  });
